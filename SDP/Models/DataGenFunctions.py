b2f6fe8db1cc3ad55e07583a3af813cf443d30d5
Fedor Tairli <fedor.tairli@adelaide.edu.au>
2025-06-26 13:38:57 +0200

    Undone the Models/Models fiasco

--- SDP/Models/DataGenFunctions.py
+++ SDP/Models/DataGenFunctions.py
@@ -30,11 +30,11 @@ def Clean_Data_DoNothing(Dataset):
 import SDP_Conv_DataGen
 
 # Data Generation Functions
-Pass_Main     = SDP_Conv_DataGen.Main_Conv2d_Grid_Charge_and_Time_DropPhiBehind
+Pass_Main     = SDP_Conv_DataGen.Main_Conv2d_Grid_Charge_and_Time
 
-Pass_Aux      = SDP_Conv_DataGen.Aux_Descriptors_DropPhiBehind
+Pass_Aux      = SDP_Conv_DataGen.Aux_Descriptors
 
-Pass_Truth    = SDP_Conv_DataGen.Truth_Just_SDP_single_DropPhiBehind
+Pass_Truth    = SDP_Conv_DataGen.Truth_SDP_via3vector
 
 Pass_Rec      = Pass_Rec_DoNothing
 
